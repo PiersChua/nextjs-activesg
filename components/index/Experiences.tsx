801edb62dadb5c5db87b44c1faa483cc071b064c
Piers Chua <2201999F@student.tp.edu.sg>
2024-09-09 15:21:28 +0800

    fixed infite scroll issue

--- components/index/Experiences.tsx
+++ components/index/Experiences.tsx
@@ -9,24 +9,49 @@ const loopScroll = keyframes`
   to {
     transform: translate(${`calc(-50% - 1rem)`});
   }`;
+const scrollDuration = 45;
 const Experiences = () => {
   const doubledImages = [...images, ...images];
   return (
     <Box bg="var(--beige-shade-1)" py="5em">
       <Container maxW="1400px">
-        <Text textStyle="h1">Shared Experiences</Text>
-        <Box overflow="hidden" bg="inherit">
+        <Text mb="0.5em" textStyle="h1">
+          Shared Experiences
+        </Text>
+        <Box overflow="hidden" bg="inherit" position="relative">
+          <Box
+            position="absolute"
+            top="0"
+            bottom="0"
+            left="0"
+            width="30px"
+            bgGradient="linear(to-r, var(--beige-shade-1), transparent)"
+            zIndex="1"
+          />
+          <Box
+            position="absolute"
+            top="0"
+            bottom="0"
+            right="0"
+            width="30px"
+            bgGradient="linear(to-l, var(--beige-shade-1), transparent)"
+            zIndex="1"
+          />
           <Flex
+            _hover={{
+              animationPlayState: "paused",
+            }}
             bg="inherit"
             w="max-content"
             gap={8} // 2rem gap
-            animation={`${loopScroll} 15s linear infinite`}
+            animation={`${loopScroll} ${scrollDuration}s linear infinite`}
           >
             {doubledImages.map((image, index) => (
               <Image
                 objectFit="cover"
                 rounded="xl"
-                h="200px"
+                h="25vw"
+                maxH="200px"
                 key={index}
                 src={image}
                 aria-hidden={index >= images.length}
